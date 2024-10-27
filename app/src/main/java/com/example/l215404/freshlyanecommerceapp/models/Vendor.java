ada85ed85b8a3deec022c4f341d50f6ea9565128
Urwa Ihtesham <urwaihtesham@gmail.com>
2024-10-27 19:12:11 +0500

    master: created app structure & dependencies

---
+++ app/src/main/java/com/example/l215404/freshlyanecommerceapp/models/Vendor.java
@@ -0,0 +1,10 @@
+package com.example.l215404.freshlyanecommerceapp.models;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tablename = "vendors")
+public class Vendor {
+    @PrimaryKey(autoGenerate = true)
+    
+}
